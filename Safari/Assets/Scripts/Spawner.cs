815d1e1e06d32fd9b1c47b3a5fb7d963b0b9f9b9
Mikhail Paulyshka <me@mixaill.tk>
2015-02-01 01:22:13 +0300

    Add Simple spawner

---
+++ Safari/Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+	
+	public float spawnTime = 5f;		// The amount of time between each spawn.
+	public float spawnDelay = 3f;		// The amount of time before spawning starts.
+	public GameObject[] enemies;		// Array of enemy prefabs.
+
+	//FIXME: unhardcode variables
+	public float xMin = 0;
+	public float xMax = 1;
+	public float yMin = 0;
+	public float yMax = 1;
+	public float z    = 4;
+
+	void Start ()
+	{
+		// Start calling the Spawn function repeatedly after a delay .
+		InvokeRepeating("Spawn", spawnDelay, spawnTime);
+	}
+	
+	void Spawn ()
+	{
+		// Instantiate a random enemy.
+		transform.position = new Vector3 (Random.Range (xMin, xMax), Random.Range (xMin, xMax), z);
+		int enemyIndex = Random.Range(0, enemies.Length);
+		Instantiate(enemies[enemyIndex], transform.position, transform.rotation);
+	}
+}

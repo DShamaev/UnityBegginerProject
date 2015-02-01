2b3b790609612e45eb6aca452e531a9b62f88833
Mikhail Paulyshka <me@mixaill.tk>
2015-02-01 17:57:57 +0300

    cleanup spawner script

--- Safari/Assets/Scripts/Spawner.cs
+++ Safari/Assets/Scripts/Spawner.cs
@@ -1,30 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Spawner.
+/// Spawn GameObjects in some radius around spawner
+/// </summary>
 public class Spawner : MonoBehaviour {
-	
-	public float spawnTime = 5f;		// The amount of time between each spawn.
-	public float spawnDelay = 3f;		// The amount of time before spawning starts.
-	public GameObject[] enemies;		// Array of enemy prefabs.
 
-	//FIXME: unhardcode variables
-	public float xMin = 0;
-	public float xMax = 1;
-	public float yMin = 0;
-	public float yMax = 1;
-	public float z    = 4;
+	public float horizontalDistribution = 1;
+	public float verticalDistribution = 1;
+
+	/// <summary>The amount of time between each spawn.</summary>
+	public float spawnTime = 5f;
+
+	/// <summary>The amount of time before spawning starts.</summary>
+	public float spawnDelay = 3f;
+
+	/// <summary>Array of objects prefabs.</summary>
+	public GameObject[] enemies;
+
+	float x,y,z;
 
 	void Start ()
 	{
+		x = transform.position.x;
+		y = transform.position.y;
+		z = transform.position.z;
+
 		// Start calling the Spawn function repeatedly after a delay .
 		InvokeRepeating("Spawn", spawnDelay, spawnTime);
 	}
 	
 	void Spawn ()
 	{
-		// Instantiate a random enemy.
-		transform.position = new Vector3 (Random.Range (xMin, xMax), Random.Range (xMin, xMax), z);
+		//Determine object spawn position
+		var spawnPosition = new Vector3 (x+Random.Range (-horizontalDistribution, horizontalDistribution), y+Random.Range (-verticalDistribution, verticalDistribution), z);
+
+		//Select an object from array to instantiate
 		int enemyIndex = Random.Range(0, enemies.Length);
-		Instantiate(enemies[enemyIndex], transform.position, transform.rotation);
+
+		// Instantiate object.
+		Instantiate(enemies[enemyIndex], spawnPosition, transform.rotation);
 	}
 }

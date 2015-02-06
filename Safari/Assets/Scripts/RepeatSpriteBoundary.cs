38e832d265040df3955be87a46aa4077fc30911d
DeomonRUS <Deomon@ya.ru>
2015-02-06 22:35:53 +0300

    tile

--- Safari/Assets/Scripts/RepeatSpriteBoundary.cs
+++ Safari/Assets/Scripts/RepeatSpriteBoundary.cs
@@ -14,8 +14,8 @@ public class RepeatSpriteBoundary : MonoBehaviour {
 		// Get the current sprite with an unscaled size
 		sprite = GetComponent<SpriteRenderer>();
 		Vector2 spriteSize = new Vector2(sprite.bounds.size.x / transform.localScale.x, sprite.bounds.size.y / transform.localScale.y);
-		
-		// Generate a child prefab of the sprite renderer
+
+ 		// Generate a child prefab of the sprite renderer
 		GameObject childPrefab = new GameObject();
 		SpriteRenderer childSprite = childPrefab.AddComponent<SpriteRenderer>();
 		childPrefab.transform.position = transform.position;
@@ -23,13 +23,17 @@ public class RepeatSpriteBoundary : MonoBehaviour {
 		
 		// Loop through and spit out repeated tiles
 		GameObject child;
-		for (int i = 1, l = (int)Mathf.Round(sprite.bounds.size.y); i < l; i++) {
-			child = Instantiate(childPrefab) as GameObject;
-			child.transform.position = transform.position - (new Vector3(0, spriteSize.y, 0) * i);
-			child.transform.parent = transform;
+		for (int i = 1; i<5; i++)  {
+			for (int j= 1; j<5; j++){
+				child = Instantiate (childPrefab) as GameObject;
+				child.transform.position = transform.position + (new Vector3(spriteSize.x*i,spriteSize.y*j, +9));
+										
+
+				child.transform.parent = transform;
+			}
 		}
-		
 		// Set the parent last on the prefab to prevent transform displacement
+		//for (int i = 0, l = (int)Mathf.Round(sprite.bounds.size.y); i < l; i++) 
 		childPrefab.transform.parent = transform;
 		
 		// Disable the currently existing sprite component since its now a repeated image

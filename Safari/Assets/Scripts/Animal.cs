028f18ae44a14e18ae9425e5a1e9e6685d8918b8
DShamaev <Dmitriy.Shamaev@dataart.com>
2015-02-08 12:33:10 +0200

    animal abstract class

---
+++ Safari/Assets/Scripts/Animal.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animal : GameObject {
+
+	public enum AnimalState{
+		IDLE, //do nothing
+		MOVING,
+	};
+
+	protected Vector3 directionVector; // will be used for scanning and movement
+	protected AnimalState currentState;
+
+	//function for instantiation and adding to scene
+	//will have same implementation in parent
+	//similar to GetInstance
+	public static Animal Spawn(){
+		Animal instance = null;
+		return instance;
+	}
+
+	//animal already has position variable in this.transform.position
+	//so we need move function
+	public abstract void Move(){
+
+	}
+
+	public abstract void ScanArea(){
+	
+	}
+
+	public abstract void changeState(AnimalState newState){
+	
+	}
+}
